c6ef6db0554f408a5f00f2c36444e2f41d931133
Adam Hellberg <sharparam@sharparam.com>
2015-03-07 22:02:26 +0100

    Add some Razer constructs.

---
+++ Colore/Razer/Keyboard/Effect.cs
@@ -0,0 +1,40 @@
+﻿namespace Colore.Razer.Keyboard
+{
+    public enum Effect
+    {
+        /// <summary>
+        /// No effect.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Wave effect.
+        /// </summary>
+        Wave,
+
+        /// <summary>
+        /// Spectrum cycling effect.
+        /// </summary>
+        SpectrumCycling,
+
+        /// <summary>
+        /// Breathing effect.
+        /// </summary>
+        Breathing,
+
+        /// <summary>
+        /// Reactive effect.
+        /// </summary>
+        Reactive,
+
+        /// <summary>
+        /// Static effect.
+        /// </summary>
+        Static,
+
+        /// <summary>
+        /// Invalid effect.
+        /// </summary>
+        Invalid
+    };
+}
