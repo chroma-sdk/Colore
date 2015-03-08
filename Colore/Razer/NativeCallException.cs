2f7b51978f174236bc61ec09b2cac94dbada9820
Adam Hellberg <sharparam@sharparam.com>
2015-03-08 04:31:08 +0100

    Add exception type for native call failures.

---
+++ Colore/Razer/NativeCallException.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------------------------------
+// <copyright file="NativeCallException.cs" company="">
+//     Copyright © 2015 by Adam Hellberg and Brandon Scott.
+// 
+//     Permission is hereby granted, free of charge, to any person obtaining a copy of
+//     this software and associated documentation files (the "Software"), to deal in
+//     the Software without restriction, including without limitation the rights to
+//     use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+//     of the Software, and to permit persons to whom the Software is furnished to do
+//     so, subject to the following conditions:
+// 
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+// 
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//     CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+//     Disclaimer: Colore is in no way affiliated with Razer and/or any of its employees
+//     and/or licensors. Adam Hellberg and Brandon Scott do not take responsibility
+//     for any harm caused, direct or indirect, to any Razer peripherals
+//     via the use of Colore.
+// 
+//     "Razer" is a trademark of Razer USA Ltd.
+// </copyright>
+// ---------------------------------------------------------------------------------------
+namespace Colore.Razer
+{
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.Runtime.Serialization;
+
+    public class NativeCallException : ColoreException
+    {
+        private readonly string _function;
+
+        private readonly Result _result;
+
+        public NativeCallException(string function, Result result)
+            : base(
+                string.Format(
+                    CultureInfo.InvariantCulture,
+                    "Call to native Chroma API function {0} failed with error: {1}",
+                    function,
+                    result),
+                new Win32Exception(result))
+        {
+            _function = function;
+            _result = result;
+        }
+
+        protected NativeCallException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public string Function { get { return _function; } }
+
+        public Result Result { get { return _result; } }
+    }
+}
