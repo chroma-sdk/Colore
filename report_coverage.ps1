cc722fa86b310caaa969aa3c7b1dd7a5dd106dc4
Adam Hellberg <sharparam@sharparam.com>
2015-03-13 23:14:14 +0100

    Fix parameter quotes not properly escaped in report_coverage.ps1

--- report_coverage.ps1
+++ report_coverage.ps1
@@ -16,5 +16,5 @@ $dll = "Corale.Colore.Tests\bin\$Configuration\Corale.Colore.Tests.dll"
 $nunit = "packages\NUnit.Runners.2.6.4\tools\nunit-console.exe"
 $filter = "+[Corale.Colore*]* -[*Tests]* -[*]Corale.Colore.Native* -[*]*NativeMethods -[*]*NativeWrapper"
 
-.\packages\OpenCover.4.5.3723\OpenCover.Console.exe -register:user -filter:"$filter" -target:"$nunit" -targetargs:"/noshadow /domain:single $dll" -output:coverage.xml
+.\packages\OpenCover.4.5.3723\OpenCover.Console.exe -register:user -filter:`"$filter`" -target:`"$nunit`" -targetargs:`"/noshadow /domain:single $dll`" -output:coverage.xml
 .\packages\coveralls.io.1.2.2\tools\coveralls.net.exe --opencover coverage.xml
