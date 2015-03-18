01585d10d9b76b6a969b81002dddcd44dc376b2a
Adam Hellberg <sharparam@sharparam.com>
2015-03-18 21:05:28 +0100

    Fix empty arrays being sent to CustomGrid constructor.

--- Corale.Colore/Core/Keyboard.cs
+++ Corale.Colore/Core/Keyboard.cs
@@ -93,9 +93,11 @@ namespace Corale.Colore.Core
                 _custom[i] = new Custom { Color = Color.Black, Key = key };
             }
 
-            _grid = new CustomGrid(new Color[Constants.MaxRows][]);
-            for (var row = 0; row < _grid.Colors.GetLength(0); row++)
-                _grid.Colors[row] = new Color[Constants.MaxColumns];
+            var gridArray = new Color[Constants.MaxRows][];
+            for (var i = 0; i < Constants.MaxRows; i++)
+                gridArray[i] = new Color[Constants.MaxColumns];
+
+            _grid = new CustomGrid(gridArray);
         }
 
         /// <summary>
