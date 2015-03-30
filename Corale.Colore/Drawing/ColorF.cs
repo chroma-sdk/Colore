fea9960675ec7ac04f5c34c8be7f4a43941c239e
Chris Bishop <christopherb6041@gmail.com>
2015-03-30 14:02:58 -0500

    Added everything from ChromaGraphics to Colore.

---
+++ Corale.Colore/Drawing/ColorF.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corale.Colore.Drawing
+{
+    public class ColorF
+    {
+
+        private float a, r, g, b;
+
+        /// <summary>
+        ///     Construct a ARGB color.
+        /// </summary>
+        /// <param name="a">Alpha component.</param>
+        /// <param name="r">Red component.</param>
+        /// <param name="g">Green component.</param>
+        /// <param name="b">Blue component.</param>
+        public ColorF(float a, float r, float g, float b)
+        {
+            this.a = a;
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        /// <summary>
+        ///     Construct a RGB color where A = 1.
+        /// </summary>
+        /// <param name="r">Red component.</param>
+        /// <param name="g">Green component.</param>
+        /// <param name="b">Blue component.</param>
+        public ColorF(float r, float g, float b)
+            : this(1f, r, g, b)
+        {
+        }
+
+        /// <summary>
+        ///     Construct a ARGB color from another ColorF with a different alpha.
+        ///     NOTE: This was created for chaining.
+        /// </summary>
+        /// <param name="a">Alpha component</param>
+        /// <param name="color"></param>
+        public ColorF(float a, ColorF color)
+            : this(a, color.R, color.G, color.B)
+        {
+        }
+
+        /// <summary>
+        ///     Construct from System.Drawing.Color with specific alpha.
+        /// </summary>
+        /// <param name="a">Alpha component.</param>
+        /// <param name="color"></param>
+        public ColorF(float a, System.Drawing.Color color)
+            : this(a, color.R / 255f, color.G / 255f, color.B / 255f)
+        {
+        }
+
+        /// <summary>
+        ///     Construct from System.Drawing.Color
+        /// </summary>
+        /// <param name="color"></param>
+        public ColorF(System.Drawing.Color color)
+            : this(color.A / 255f, color)
+        {
+        }
+
+        /// <summary>
+        ///     Construct from Corale.Colore.Core.Color with a specific alpha.
+        /// </summary>
+        /// <param name="a">Alpha component</param>
+        /// <param name="color"></param>
+        public ColorF(float a, Corale.Colore.Core.Color color)
+            : this(a, color.R / 255f, color.G / 255f, color.B / 255f)
+        {
+        }
+
+        /// <summary>
+        ///     Construct from Corale.Colore.Core.Color.
+        /// </summary>
+        /// <param name="color"></param>
+        public ColorF(Corale.Colore.Core.Color color)
+            : this(1, color)
+        {
+        }
+
+        /// <summary>
+        ///     Alpha component.
+        /// </summary>
+        public float A
+        {
+            get { return a; }
+            set
+            {
+                a = value;
+                if (a > 1)
+                {
+                    a = 1;
+                }
+                else if (a < 0)
+                {
+                    a = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Red component.
+        /// </summary>
+        public float R
+        {
+            get { return r; }
+            set
+            {
+                r = value;
+                if (r > 1)
+                {
+                    r = 1;
+                }
+                else if (r < 0)
+                {
+                    r = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Green component.
+        /// </summary>
+        public float G
+        {
+            get { return g; }
+            set
+            {
+                g = value;
+                if (g > 1)
+                {
+                    g = 1;
+                }
+                else if (g < 0)
+                {
+                    g = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Blue component.
+        /// </summary>
+        public float B
+        {
+            get { return b; }
+            set
+            {
+                b = value;
+                if (b > 1)
+                {
+                    b = 1;
+                }
+                else if (b < 0)
+                {
+                    b = 0;
+                }
+            }
+        }
+
+
+        /// <summary>
+        ///     Blend a color into this color by a factor of blend.
+        /// </summary>
+        /// <param name="blendTo">The color to blend with.</param>
+        /// <param name="blend">A normalized value that tells the blender how much of the other color there is.</param>
+        public ColorF Blend(ColorF blendTo, float blend)
+        {
+            float invBlend = 1 - blend;
+
+            float a = this.a * invBlend + blendTo.A * blend;
+            float r = this.r * invBlend + blendTo.R * blend;
+            float g = this.g * invBlend + blendTo.G * blend;
+            float b = this.b * invBlend + blendTo.B * blend;
+
+            return new ColorF(a, r, g, b);
+        }
+
+        /// <summary>
+        ///     Get the inverse of this color.
+        /// </summary>
+        /// <param name="includeAlpha">Whether the alpha should be inversed.</param>
+        /// <returns></returns>
+        public ColorF Inverse(bool includeAlpha)
+        {
+            if(includeAlpha)
+            {
+                return new ColorF(1 - a, 1- r, 1 - g, 1 - b);
+            }
+            else
+            {
+                return new ColorF(a, 1 - r, 1 - g, 1 - b);
+            }
+        }
+
+        /// <summary>
+        ///     Get the inverse of the color. Alpha stays the same.
+        /// </summary>
+        /// <returns></returns>
+        public ColorF Inverse()
+        {
+            return Inverse(false);
+        }
+
+
+        /// <summary>
+        ///     Constructs a System.Drawing.Color from the ARGB components.
+        /// </summary>
+        /// <returns>System.Drawing.Color identical to this color.</returns>
+        public System.Drawing.Color ToSystemColor()
+        {
+            return System.Drawing.Color.FromArgb((int) (a * 255), (int) (r * 255), (int) (g * 255), (int) (b * 255));
+        }
+
+        /// <summary>
+        ///     Constructs a Corale.Colore.Core.Color from the RGB components.
+        /// </summary>
+        /// <returns>Corale.Colore.Core.Color identical to this color, minus the alpha.</returns>
+        public Corale.Colore.Core.Color ToColor()
+        {
+            return new Corale.Colore.Core.Color(r, g, b);
+        }
+
+        /// <summary>
+        ///     Constructs a Corale.Colore.Core.Color from the RGB components
+        ///     and blends it to the blendTo color with the alpha.
+        /// </summary>
+        /// <param name="blendTo">The color to blend with.</param>
+        /// <returns>Corale.Colore.Core.Color identical to this color, blended with alpha.</returns>
+        public Corale.Colore.Core.Color ToColor(Corale.Colore.Core.Color blendTo)
+        {
+            return new ColorF(blendTo).Blend(this, this.A).ToColor();
+        }
+
+    }
+}
---
+++ Corale.Colore/Drawing/Core.cs
@@ -0,0 +1,110 @@
+﻿using Corale.Colore.Core;
+using Corale.Colore.Razer.Keyboard;
+using Corale.Colore.Razer.Keyboard.Effects;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corale.Colore.Drawing
+{
+    public partial class ChromaGraphics
+    {
+
+        public const int LogoX = 20;
+        public const int LogoY = 0;
+
+        private Color[][] grid;
+
+        public ChromaGraphics()
+        {
+            // Create the proper grid.
+            grid = new Color[Constants.MaxRows][];
+            for (int i = 0; i < grid.Length; i++)
+            {
+                grid[i] = new Color[Constants.MaxColumns];
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    grid[i][j] = Color.Black;
+                }
+            }
+        }
+
+
+        /// <summary>
+        ///     Width of the graphics object.
+        /// </summary>
+        public int Width
+        {
+            get { return grid[0].Length; }
+        }
+
+        /// <summary>
+        ///     Height of the graphics object
+        /// </summary>
+        public int Height
+        {
+            get { return grid.Length; }
+        }
+
+
+
+        /// <summary>
+        ///     Get the color of any key on the keyboard.
+        /// </summary>
+        /// <param name="x">X position of the key.</param>
+        /// <param name="y">Y position of the key</param>
+        /// <returns>The color of the key at that position.</returns>
+        public ColorF GetColor(int x, int y)
+        {
+            if ((x >= 0 && x < Width) && (y >= 0 && y < Height))
+            {
+                Color color = grid[y][x];
+                return new ColorF(color);
+            }
+            else
+            {
+                throw new ArgumentException("The x or y component is out of range.");
+            }
+        }
+
+        /// <summary>
+        ///     Set the color of any key. Cannot set the logo color with this method.
+        /// </summary>
+        /// <param name="x">X position of the key</param>
+        /// <param name="y">Y position of the key</param>
+        /// <param name="color">Color the key will be set to.</param>
+        public void SetColor(int x, int y, ColorF color)
+        {
+            if ((x >= 0 && x < Width)
+                && (y >= 0 && y < Height)
+                && !(x == LogoX && y == LogoY)) // Do not override the logo with this method.
+            {
+
+                grid[y][x] = color.ToColor(grid[y][x]);
+            }
+        }
+
+        /// <summary>
+        ///     Set the color of the logo.
+        /// </summary>
+        /// <param name="color">Color the logo will be set to.</param>
+        public void SetLogoColor(ColorF color)
+        {
+            grid[LogoY][LogoX] = color.ToColor(grid[LogoY][LogoX]);
+        }
+
+
+
+        /// <summary>
+        ///     The grid effect that can be set on any keyboard.
+        /// </summary>
+        public CustomGrid Effect
+        {
+            get { return new CustomGrid(grid); }
+        }
+
+    }
+}
