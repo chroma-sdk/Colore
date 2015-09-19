b7ec383e10104c454f4e64e880f9b007dd64d46f
Nicholas Martin <n.martin@movura.co.uk>
2015-09-19 21:48:59 +0100

    Felt the wrath of Adam's code ban-hammer

--- Corale.Colore/Razer/Result.cs
+++ Corale.Colore/Razer/Result.cs
@@ -214,7 +214,7 @@ namespace Corale.Colore.Razer
         /// </summary>
         /// <param name="result">An instance of the <see cref="Result" /> to convert.</param>
         /// <returns>The integer equivalent of the <paramref name="result" />.</returns>
-        public static implicit operator int (Result result)
+        public static implicit operator int(Result result)
         {
             return result._value;
         }
@@ -237,7 +237,7 @@ namespace Corale.Colore.Razer
         /// <c>true</c> if the <paramref name="result" /> represents a <c>true</c> value (success),
         /// otherwise <c>false</c> (failure).
         /// </returns>
-        public static implicit operator bool (Result result)
+        public static implicit operator bool(Result result)
         {
             return result == RzSuccess;
         }
