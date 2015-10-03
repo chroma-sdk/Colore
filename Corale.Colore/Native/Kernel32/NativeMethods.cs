39dd013ecbbabc292b7e7f88d8c4f890c70e224e
Adam Hellberg <sharparam@sharparam.com>
2015-10-03 23:21:15 +0200

    Fix import attributes on Kernel32 functions

--- Corale.Colore/Native/Kernel32/NativeMethods.cs
+++ Corale.Colore/Native/Kernel32/NativeMethods.cs
@@ -43,16 +43,18 @@ namespace Corale.Colore.Native.Kernel32
         /// </summary>
         private const string DllName = "kernel32.dll";
 
-        [DllImport(DllName, CharSet = CharSet.Auto, EntryPoint = "GetProcAddress", SetLastError = true)]
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "GetProcAddress", ExactSpelling = true,
+            SetLastError = true, BestFitMapping = false)]
         internal static extern IntPtr GetProcAddress(IntPtr module, string procName);
 
-        [DllImport(DllName, CharSet = CharSet.Auto, EntryPoint = "LoadLibrary", SetLastError = true)]
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "LoadLibrary", SetLastError = true)]
         internal static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string filename);
 
-        [DllImport(DllName, CharSet = CharSet.Auto, EntryPoint = "GetCurrentProcess", SetLastError = true)]
+        [DllImport(DllName, EntryPoint = "GetCurrentProcess", SetLastError = true)]
         internal static extern IntPtr GetCurrentProcess();
 
-        [DllImport(DllName, CharSet = CharSet.Auto, EntryPoint = "GetModuleHandle", SetLastError = true)]
+        [DllImport(DllName, CharSet = CharSet.Ansi, EntryPoint = "GetModuleHandle", SetLastError = true,
+            BestFitMapping = false)]
         internal static extern IntPtr GetModuleHandle(string moduleName);
 
         [DllImport(DllName, EntryPoint = "IsWow64Process", SetLastError = true,
