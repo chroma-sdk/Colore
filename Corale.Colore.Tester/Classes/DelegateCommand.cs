b0bf302696e94f9c475bf237b682cbf714ae8307
brandonscott <brandon@brandonscott.co.uk>
2015-12-21 19:31:55 +0000

    Add additional missing files.

---
+++ Corale.Colore.Tester/Classes/DelegateCommand.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Corale.Colore.Tester.Classes
+{
+    using System;
+    using System.Windows.Input;
+
+    public class DelegateCommand : ICommand
+    {
+        public DelegateCommand(Action action)
+        {
+            this.CommandAction = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public Action CommandAction { get; }
+
+        public void Execute(object parameter)
+        {
+            this.CommandAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
