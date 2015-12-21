b310971670c7a5fd574d5ef47b47c9961b44ed54
Adam Hellberg <sharparam@sharparam.com>
2015-12-21 13:54:52 +0100

    Update coverage script

--- report_coverage.ps1
+++ report_coverage.ps1
@@ -13,13 +13,13 @@ If ($Platform -ne "Any CPU")
 }
 
 $dll = "Corale.Colore.Tests\bin\$Configuration\Corale.Colore.Tests.dll"
-$nunit = "packages\NUnit.Runners.2.6.4\tools\nunit-console.exe"
+$nunit = "packages\NUnit.Console.3.0.1\tools\nunit3-console.exe"
 $filter = "+[Corale.Colore*]* -[*Tests]* -[*]*Constants -[*]Corale.Colore.Native* -[*]*NativeMethods -[*]*NativeWrapper -[*]Corale.Colore.Annotations*"
-$targetArgs = "/noshadow /domain:single $dll"
+$targetArgs = "$dll"
 
 $Env:NUNIT_EXEC = $nunit
 $Env:OPENCOVER_FILTER = $filter
 $Env:TARGET_ARGS = $targetArgs
 
-.\packages\OpenCover.4.5.3723\OpenCover.Console.exe --% -register:user -filter:"%OPENCOVER_FILTER%" -target:"%NUNIT_EXEC%" -targetargs:"%TARGET_ARGS%" -output:coverage.xml
-.\packages\coveralls.io.1.2.2\tools\coveralls.net.exe --opencover coverage.xml
+.\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe --% -register:user -filter:"%OPENCOVER_FILTER%" -target:"%NUNIT_EXEC%" -targetargs:"%TARGET_ARGS%" -output:coverage.xml
+.\packages\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --% --opencover -i coverage.xml --repoTokenVariable COVERALLS_REPO_TOKEN --jobId %CI_JOB_ID% --serviceName TeamCity
