cf319a87f205cb28bcd77a431ec0b666af1b2549
Adam Hellberg <sharparam@sharparam.com>
2016-05-24 22:23:28 +0200

    Enable logging to console in tester app

--- Corale.Colore.Tester/App.xaml.cs
+++ Corale.Colore.Tester/App.xaml.cs
@@ -25,10 +25,16 @@
 
 namespace Corale.Colore.Tester
 {
+    using log4net.Config;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        public App()
+        {
+            XmlConfigurator.Configure();
+        }
     }
 }
