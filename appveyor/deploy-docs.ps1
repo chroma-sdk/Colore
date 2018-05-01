53a7e20314f3680bd2146bdb8e8671a857b3bd0d
Adam Hellberg <sharparam@sharparam.com>
2018-05-01 23:39:14 +0200

    Fix AppVeyor docs script issues

--- appveyor/deploy-docs.ps1
+++ appveyor/deploy-docs.ps1
@@ -1,3 +1,8 @@
+function rmrf($path)
+{
+    Remove-Item $path -Recurse -Force -ErrorAction Ignore
+}
+
 if ($env:APPVEYOR_REPO_TAG -ne 'true' -and $env:APPVEYOR_REPO_BRANCH -ne 'develop')
 {
     echo 'Not running from tag or develop branch, aborting,'
@@ -19,20 +24,20 @@ if ($env:APPVEYOR_REPO_TAG -eq 'true')
 }
 
 echo 'Cleaning any existing gh-pages directory'
-rm -Recurse -Force gh-pages
+rmrf gh-pages
 
 echo 'Cloning gh-pages branch'
-& git clone -b gh-pages --depth 1 -- git@github.com:CoraleStudios/Colore.git gh-pages
+git clone -b gh-pages --depth 1 -- git@github.com:CoraleStudios/Colore.git gh-pages
 
 echo "Removing existing documentation at gh-pages/${target}"
-rm -Recurse -Force .\gh-pages\${target}
+rmrf .\gh-pages\${target}
 echo "Removing existing coverage at gh-pages/${covtarget}"
-rm -Recurse -Force .\gh-pages\${covtarget}
+rmrf .\gh-pages\${covtarget}
 
 echo 'Copying new documentation'
-cp .\docs\_site .\gh-pages\${target}
+cp .\docs\_site .\gh-pages\${target} -Recurse
 echo 'Copying new coverage'
-cp .\artifacts\coverage-report .\gh-pages\${covtarget}
+cp .\artifacts\coverage-report .\gh-pages\${covtarget} -Recurse
 
 $gitdata="$(git log -n 1 --format='commit %h - %s')"
 echo "Git data: ${gitdata}"
@@ -54,6 +59,6 @@ git push origin gh-pages
 Pop-Location
 
 echo 'Removing gh-pages folder after completion of update'
-rm -rf gh-pages
+rmrf gh-pages
 
 echo 'AppVeyor docs deploy script finished'
