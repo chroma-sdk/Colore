6fa699ef498069aeda1fe027b0cf3bab3af7a6f4
Adam Hellberg <sharparam@sharparam.com>
2020-06-18 17:59:38 +0200

    Fix parameter parsing in build.sh

--- build.sh
+++ build.sh
@@ -2,23 +2,25 @@
 
 # Define default arguments.
 SCRIPT="build.cake"
+TARGET="Default"
 VERBOSITY="Normal"
 CAKE_ARGUMENTS=""
 
 # Parse arguments.
 for i in "$@"; do
     case $1 in
-        -s|--script) SCRIPT="$2"; shift ;;
-        -V|--verbosity) VERBOSITY="$2"; shift ;;
-        --) shift; CAKE_ARGUMENTS="${CAKE_ARGUMENTS} $@"; break ;;
+        -s|--script) SCRIPT="$2"; shift $(( $# > 0 ? 1 : 0 )) ;;
+        -t|--target) TARGET="$2"; shift $(( $# > 0 ? 1 : 0 )) ;;
+        -V|--verbosity) VERBOSITY="$2"; shift $(( $# > 0 ? 1 : 0 )) ;;
+        --) shift $(( $# > 0 ? 1 : 0 )); CAKE_ARGUMENTS="${CAKE_ARGUMENTS} $@"; break ;;
         *) CAKE_ARGUMENTS="${CAKE_ARGUMENTS} $1" ;;
     esac
-    shift
+    shift $(( $# > 0 ? 1 : 0 ))
 done
 set -- ${CAKE_ARGUMENTS}
 
 # Restore Cake tool
-dotnet tool restore
+#dotnet tool restore
 
 if [ $? -ne 0 ]; then
     echo "An error occured while installing Cake."
@@ -26,6 +28,5 @@ if [ $? -ne 0 ]; then
 fi
 
 # Start Cake
-
 dotnet tool run dotnet-cake --bootstrap --verbosity=$VERBOSITY
-dotnet tool run dotnet-cake "--" "$SCRIPT" "$@" --verbosity=$VERBOSITY
+dotnet tool run dotnet-cake "--" "$SCRIPT" --target="$TARGET" "$@" --verbosity=$VERBOSITY
